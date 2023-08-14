e163e5d9ba78cb8f67924ec5466b08dd298f1195
Dylan-Zupec <135743818+Dylan-Zupec@users.noreply.github.com>
2023-08-13 20:08:26 -0500

    fixed spacing in lcd_controller_private.h

--- Inc/lcd_controller_private.h
+++ Inc/lcd_controller_private.h
@@ -10,39 +10,39 @@
 #include "task.h"
 #include "queue.h"
 
-#define LCD_CLEAR 			0x1
-#define LCD_HOME 			0x2
+#define LCD_CLEAR 		0x1
+#define LCD_HOME 		0x2
 #define LCD_ENTRY_MODE		0x4
 #define LCD_DISPLAY_CTRL	0x8
-#define LCD_SHIFT			0x10
+#define LCD_SHIFT		0x10
 #define LCD_FUNC_SET		0x20
 #define LCD_CURSOR_SET		0x80
 
 /**********Instruction Flags**********/
 /*LCD_ENTRY_MODE*/
-#define LCD_AUTO_INCREMENT	 	0x2
-#define LCD_AUTO_DECREMENT	 	0
+#define LCD_AUTO_INCREMENT	0x2
+#define LCD_AUTO_DECREMENT	0
 #define LCD_AUTO_SHIFT_DISPLAY 	0x1
 #define LCD_AUTO_SHIFT_CURSOR	0
 /*LCD_DISPLAY_CTRL*/
-#define LCD_DISPLAY_ON			0x4
-#define LCD_DISPLAY_OFF			0
-#define LCD_CURSOR_ON			0x2
-#define LCD_CURSOR_OFF			0
-#define LCD_CURSOR_BLINK_ON		0x1
+#define LCD_DISPLAY_ON		0x4
+#define LCD_DISPLAY_OFF		0
+#define LCD_CURSOR_ON		0x2
+#define LCD_CURSOR_OFF		0
+#define LCD_CURSOR_BLINK_ON	0x1
 #define LCD_CURSOR_BLINK_OFF	0
 /*LCD_SHIFT*/
-#define LCD_SHIFT_DISPLAY		0x8
-#define LCD_SHIFT_CURSOR		0
-#define LCD_SHIFT_RIGHT			0x4
-#define LCD_SHIFT_LEFT			0
+#define LCD_SHIFT_DISPLAY	0x8
+#define LCD_SHIFT_CURSOR	0
+#define LCD_SHIFT_RIGHT		0x4
+#define LCD_SHIFT_LEFT		0
 /*LCD_FUNC_SET*/
-#define LCD_8_BIT_MODE			0x10
-#define LCD_4_BIT_MODE			0
-#define LCD_2_LINE_MODE			0x8
-#define LCD_1_LINE_MODE			0
-#define LCD_5x11_FONT			0x4
-#define LCD_5x8_FONT			0
+#define LCD_8_BIT_MODE		0x10
+#define LCD_4_BIT_MODE		0
+#define LCD_2_LINE_MODE		0x8
+#define LCD_1_LINE_MODE		0
+#define LCD_5x11_FONT		0x4
+#define LCD_5x8_FONT		0
 /*************************************/
 
 typedef enum {LCD_REG_INSTRUCTION, LCD_REG_DATA} LCD_Register_e;
